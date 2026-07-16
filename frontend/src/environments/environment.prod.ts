58222a20abc30fbaea26b6fe6e6a94556bcba57c
Jashim Gafoor <jashi202jg@gmail.com>
2026-07-16 08:50:02 +0530

    fixes

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 // Environment configuration for production
 export const environment = {
   production: true,
-  apiUrl: 'http://your-production-api.com/api',
-  rapidApiKey: '0eb71abbf3mshd1b66aa317c31aap118931jsnb158a2ee05b7'
+  apiUrl: 'https://referiqo.onrender.com/api',
 };
